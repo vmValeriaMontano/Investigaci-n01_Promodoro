26c589f61cf13fc487419632ca6ea5b60f1e6c24
valeria.montano <montanovaleria55@gmail.com>
2026-07-14 15:24:39 -0600

    activación de View Biding

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,6 +31,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    /*Aqui activasmos la parte de viewBinding
+    esto para no usar el findViewById*/
+    buildFeatures{
+        viewBinding = true
+    }
 }
 
 dependencies {
